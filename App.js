3f7c8504bfc8fff5652745630b3321728c81260d
souzafeb <souzafeb@gmail.com>
2023-09-16 20:35:18 -0400

    Caretaker/Children Profiles added

--- App.js
+++ App.js
@@ -10,16 +10,13 @@ import { PaperProvider } from 'react-native-paper';
 // PAGE IMPORTS //
 import SignUp from './app/screens/SignUp';
 import SignIn from "./app/screens/SignIn";
-import AddCaretaker from './app/screens/AddCaretaker';
-import AddChild from './app/screens/AddChild';
+import CaretakerProfile from './app/screens/CaretakerProfile';
+import ChildProfile from './app/screens/ChildProfile';
 
 // COMPONENT IMPORTS //
 import NavigationBar from "./app/components/NavigationBar";
 import { useAuthenticationStore } from "./app/stores/AuthenticationStore";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import Settings from "./app/screens/Settings";
-import Home from "./app/screens/Home";
-import History from "./app/screens/History";
 
 const Stack = createNativeStackNavigator();
 
@@ -48,8 +45,8 @@ const App = () => {
                         isSignedIn ?
                             <>
                                 <Stack.Screen name="Navigation" component={NavigationBar} options={styles.header} />
-                                <Stack.Screen name="AddCaretaker" component={AddCaretaker} options={styles.headerWithNav} />
-                                <Stack.Screen name="AddChild" component={AddChild} options={styles.headerWithNav} />
+                                <Stack.Screen name="CaretakerProfile" component={CaretakerProfile} options={styles.headerWithNav} />
+                                <Stack.Screen name="ChildProfile" component={ChildProfile} options={styles.headerWithNav} />
                             </> :
                             <>
                                 <Stack.Screen name="SignIn" component={SignIn} options={styles.header} />
